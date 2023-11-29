190cee215ba32bd819e9c046f5b10a88ef707d6e
fayzull0h <fayzulloh2004@gmail.com>
2023-11-29 14:56:05 -0500

    Created basic main file to display a window and resize it

---
+++ main.cc
@@ -0,0 +1,31 @@
+#include <ncurses.h>
+
+int main() {
+    int startx = 0, starty = 1, width = 80, height = 22;
+    int ch;
+    WINDOW *win;
+
+    initscr();
+    cbreak();
+
+    keypad(stdscr, TRUE);
+
+    printw("Press F1 to exit");
+    refresh();
+
+    win = newwin(height, width, starty, startx);
+    wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+    wrefresh(win);
+
+    while ((ch = getch()) != KEY_F(1)) {
+        wborder(win, ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ');
+        wrefresh(win);
+        delwin(win);
+        win = newwin(++height, ++width, starty, startx);
+        wborder(win, '|', '|', '-', '-', '+', '+', '+', '+');
+        wrefresh(win);
+    }
+
+    endwin();
+    return 0;
+}
